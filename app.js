55419f3861c2c5a2a432a2162519c510473feb87
Dogan <dogan2002@hotmail.be>
2026-06-15 22:42:31 +0200

    feat: use textContent to display the day

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const allCards = document.querySelectorAll(".day-card");
 
 function changeColor(e){
     e.currentTarget.classList.toggle("selected");
+
+    console.log(e.currentTarget.querySelector("h3").textContent);
 }
 
 allCards.forEach(card =>{
