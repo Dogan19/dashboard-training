295ebef76edb50ea282c9b495f033a1bc91bf0ce
Dogan <dogan2002@hotmail.be>
2026-06-16 16:00:33 +0200

    feat: add workout-content id to display the program of the day selected under the title

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const allCards = document.querySelectorAll(".day-card");
+const workoutContent = document.querySelector("#workout-content");
+const workouts = {
+    "Monday": "Chest/Biceps",
+    "Tuesday": "Street(Push/Full Shoulders",
+    "Wednesday": "Back/Triceps",
+    "Thursday": "Cardio",
+    "Friday": "Legs",
+    "Saturday": "Street Full Body/Core",
+    "Sunday": "Complete rest"
+};
 
 function handleDayClick(e){
+    let clickedDay;
+    
     e.currentTarget.classList.toggle("selected");
 
-    document.querySelector("h2").textContent = e.currentTarget.querySelector("h3").textContent;
+    clickedDay = e.currentTarget.querySelector("h3").textContent;
+    workoutContent.textContent = workouts[clickedDay];
 }
 
 allCards.forEach(card =>{
