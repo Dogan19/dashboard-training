3f77645573110e8c5467d651565def4ebe6ffb62
Dogan <dogan2002@hotmail.be>
2026-06-29 17:42:29 +0200

    feature: save weightHistory on localStorage

--- app.js
+++ app.js
@@ -21,6 +21,12 @@ const WEIGHT_MAX = 100;
 const FASTING_MIN = 0;
 const FASTING_MAX = 24;
 
+//LocalStorage
+let weightHistory = JSON.parse(localStorage.getItem("historiquePoids")) || [];
+    if (weightHistory.length > 0){
+        document.querySelector("#weight-value").textContent = weightHistory[weightHistory.length - 1]
+    }
+
 // Functions
 function handleDayClick(e){
     let clickedDay;
@@ -50,6 +56,8 @@ function handleStatsClick(){
 
     if (!isNaN(newWeight) && newWeight<WEIGHT_MAX && newWeight>WEIGHT_MIN) {
         document.querySelector("#weight-value").textContent = newWeight;
+        weightHistory.push(newWeight);
+        localStorage.setItem("historiquePoids", JSON.stringify(weightHistory));
     } else {
         alert(`Erreur : Veuillez entrer un nombre valide entre ${WEIGHT_MIN} et ${WEIGHT_MAX} (non inclus)`);
     }
