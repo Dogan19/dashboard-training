68976b116d616e3429b13cc2d918069c6bcb1bda
Dogan <dogan2002@hotmail.be>
2026-06-30 23:53:25 +0200

    feature: display today's workout dynamically

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const fastingValue = document.querySelector(".fasting-card");
 const creatineValue = document.querySelector(".creatine-card");
 const creatineDate = document.querySelector("#creatine-date");
 
+
 // Constants
 const workouts = {
     "Monday": "Chest/Biceps",
@@ -20,22 +21,31 @@ const WEIGHT_MIN = 30;
 const WEIGHT_MAX = 100;
 const FASTING_MIN = 0;
 const FASTING_MAX = 24;
+const DAYS_OF_WEEK = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-//LocalStorage
+
+// Initialisation & LocalStorage
 let weightHistory = JSON.parse(localStorage.getItem("historiquePoids")) || [];
-    if (weightHistory.length > 0){
-        document.querySelector("#weight-value").textContent = weightHistory[weightHistory.length - 1];
-    }
+if (weightHistory.length > 0){
+    document.querySelector("#weight-value").textContent = weightHistory[weightHistory.length - 1];
+}
+
 let savedCreatine = localStorage.getItem("historiqueCreatine");
-    if (savedCreatine){
-        creatineDate.textContent = savedCreatine;
-    }
+if (savedCreatine){
+    creatineDate.textContent = savedCreatine;
+}
+
 let fastingHistory = JSON.parse(localStorage.getItem("historiqueJeune")) || [];
-    if (fastingHistory.length > 0){
-        let lastFasting = fastingHistory[fastingHistory.length - 1];
-        let fastingLeft = FASTING_MAX - lastFasting;
-        document.querySelector("#fasting-value").textContent = `${lastFasting}/${fastingLeft}`;
-    }
+if (fastingHistory.length > 0){
+    let lastFasting = fastingHistory[fastingHistory.length - 1];
+    let fastingLeft = FASTING_MAX - lastFasting;
+    document.querySelector("#fasting-value").textContent = `${lastFasting}/${fastingLeft}`;
+}
+
+let currentDayName = DAYS_OF_WEEK[new Date().getDay()];
+let todaysWorkout = workouts[currentDayName];
+workoutContent.textContent = todaysWorkout;
+
 
 // Functions
 function handleDayClick(e){
@@ -89,6 +99,7 @@ function handleFastingClick(){
         alert(`Erreur veuillez entrer un nombre valide entre ${FASTING_MIN} et ${FASTING_MAX}`);
     }
 }
+
 
 // Event Listeners
 allCards.forEach(card =>{
